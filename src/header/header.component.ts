f82c1e2951477eba97634c0b8e6d1b00e3e4b590
João M. Gomes <joaomarquesgomes94@gmail.com>
2024-06-20 12:49:15 +0100

    update

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
 
-@Component({
-  selector: 'app-header',
-  standalone: true,
-  template: '',
-  styleUrl: './header.component.css',
-})
-export class HeaderComponent {}
+@Component({ 
+  selector: 'app-header', 
+  standalone: true, 
+  template: '<h1>{{title}}</h1>', 
+  styleUrl: './header.component.css', 
+}) 
+  
+  export class HeaderComponent { 
+    title = 'My Notes'; 
+  }
